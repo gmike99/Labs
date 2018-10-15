232068a939b021ec5f119476940a2ba6e3f2c688
gmike99 <gursky.michael1999@gmail.com>
2018-10-16 02:16:22 +0300

    db lab6 added

--- DB/Lab6/DataSet.sql
+++ DB/Lab6/DataSet.sql
@@ -8,6 +8,20 @@ club_name VARCHAR(50) DEFAULT 'Unknown',
 FOREIGN KEY (club_name) REFERENCES football_club (club_name),
 PRIMARY KEY(last_name, first_name));
 
+INSERT INTO football_player (last_name, first_name, years_of_experience, club_name) 
+VALUES 
+('Lohvinov', 'John', 5, NULL),
+('Beckenbauer', 'Yura', 4, NULL),
+('Maradona', 'Vitalik', 8, NULL),
+('Kisilov', 'Anna', 2, NULL),
+('Popov', 'Daniil', 6, NULL),
+('Ronaldo', 'Oleh', 12, NULL),
+('Herak', 'Lawrence', 4, NULL),
+('Pelé', 'John', 3, NULL),
+('Beckenbauer', 'Ostap', 3, NULL),
+('Müller', 'Jenniffer', 1, NULL);
+
+
 CREATE TABLE football_club (
 club_name VARCHAR(50) NOT NULL,
 club_owner_name VARCHAR(50) DEFAULT 'Unknown',
@@ -15,17 +29,154 @@ coach_name VARCHAR(50) DEFAULT 'Unknown',
 country VARCHAR(50) DEFAULT 'Unknown',
 PRIMARY KEY(club_name));
 
-CREATE TABLE ad_company (
+INSERT INTO football_club VALUES 
+('Manchester United', 'Lohvinov', 'Fedunushyn', 'England'),
+('Real Madrid', 'Cruyff', 'Eusébio', 'Spain'),
+('Everton FC', 'Stéfano', 'Gursky', 'Italy'),
+('Napoli', 'Garrincha', 'Prychko', 'Italy'),
+('Southampton FC', 'Maradona', 'Kisilov', 'England'),
+('West Ham United', 'Meazza', 'Maslanyi', 'England'),
+('Schalke', 'Müller', 'Popov', 'Germany'),
+('Inter Milan', 'Pelé', 'Lozhkin', 'Italy'),
+('Leicester City', 'Ronaldo', 'Ostash', 'England'),
+('Atlético Madrid', 'Manko', 'Herak', 'Spain');
+
+
+CREATE TABLE add_company (
 company_name VARCHAR(50) NOT NULL,
 budget DOUBLE DEFAULT 0.0,
 industry VARCHAR(50) DEFAULT 'Unknown',
 counry VARCHAR(50) DEFAULT 'Unknown',
 PRIMARY KEY(company_name));
 
+INSERT INTO add_company VALUES
+('Nike', 55.8, 'Clothes', 'USA'),
+('Citroen ', 86.3, 'Automotive', 'France'),
+('Opel ', 15.9, 'Automotive', 'Germany'),
+('Walmart', 45.2, 'Shops', 'USA'),
+('Puma', 14.6, 'Clothes', 'USA'),
+('Facebook', 19.8, 'IT', 'USA'),
+('Xiaomi', 8.9, 'IT', 'China'),
+('Mercedes-Benz', 125.0, 'Automotive', 'Germany'),
+('Lays', 44.2, 'Food', 'Italy'),
+('Mills Inc.', 31.1, 'Architecture', 'Netherlands');
+
+
 CREATE TABLE playerscompanies (
 last_name VARCHAR(30) NOT NULL,
 first_name VARCHAR(30) NOT NULL, 
 company_name VARCHAR(50) NOT NULL,
 FOREIGN KEY (last_name, first_name) REFERENCES football_player (last_name, first_name),
-FOREIGN KEY (company_name) REFERENCES ad_company (company_name),
+FOREIGN KEY (company_name) REFERENCES add_company (company_name),
 PRIMARY KEY(last_name, first_name, company_name));
+
+
+DELIMITER //
+
+CREATE PROCEDURE insert_playerscompanies(
+IN last_name_in VARCHAR(30),
+IN first_name_in VARCHAR(30),
+IN company_name_in VARCHAR(50)
+)
+
+BEGIN
+DECLARE msg VARCHAR(50);
+-- checks if player exists
+IF NOT EXISTS(SELECT * FROM football_player WHERE last_name = last_name_in AND first_name = first_name_in) 
+THEN SET msg = 'There is no such football player';
+-- checks if company exists
+ELSEIF NOT EXISTS(SELECT * FROM add_company WHERE company_name = company_name_in)
+THEN SET msg = 'There is no such company';
+-- checks if such combinations exists
+ELSEIF EXISTS(
+SELECT * FROM playerscompanies WHERE
+last_name = (SELECT last_name FROM football_player WHERE last_name = last_name_in AND first_name = first_name_in) AND
+first_name = (SELECT first_name FROM football_player WHERE last_name = last_name_in AND first_name = first_name_in) AND
+company_name = (SELECT company_name FROM add_company WHERE company_name = company_name_in)
+) THEN SET msg = 'This company already has this player';
+
+ELSE INSERT INTO playerscompanies VALUE(
+(SELECT last_name FROM football_player WHERE last_name = last_name_in AND first_name = first_name_in),
+(SELECT first_name FROM football_player WHERE last_name = last_name_in AND first_name = first_name_in),
+(SELECT company_name FROM add_company WHERE company_name = company_name_in)
+); SET msg = 'OK'; 
+
+END IF;
+
+SELECT msg AS message;
+
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE PROCEDURE update_football_player(
+IN old_last_name_in VARCHAR(30),
+IN old_first_name_in VARCHAR(30),
+IN new_last_name_in VARCHAR(30),
+IN new_first_name_in VARCHAR(30)
+)
+
+BEGIN
+
+DECLARE msg VARCHAR(50);
+
+IF EXISTS (SELECT * FROM football_player WHERE last_name = old_last_name_in AND first_name = old_first_name_in) 
+THEN UPDATE football_player SET last_name = new_last_name_in, first_name = new_first_name_in 
+WHERE last_name = old_last_name_in AND first_name = old_first_name_in; SET msg = 'OK';
+
+ELSE SET msg = 'No such player';
+END IF;
+SELECT msg AS message;
+
+END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE update_football_club(
+IN old_club_name_in VARCHAR(50),
+IN new_club_name_in VARCHAR(50)
+)
+
+BEGIN
+
+DECLARE msg VARCHAR(50);
+
+IF EXISTS (SELECT * FROM football_club WHERE club_name = old_club_name_in) 
+THEN UPDATE football_club SET club_name = new_club_name_in
+WHERE club_name = old_club_name_in; SET msg = 'OK';
+
+ELSE SET msg = 'No such club';
+
+END IF;
+
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE update_company(
+IN old_company_name_in VARCHAR(50),
+IN new_company_name_in VARCHAR(50)
+)
+
+BEGIN 
+
+DECLARE msg VARCHAR(50);
+
+IF EXISTS (SELECT * FROM add_company WHERE commany_name = old_company_name_in)
+THEN UPDATE add_company SET company_name = new_company_name_in
+WHERE company_name = old_company_name_in; SET msg = 'OK';
+
+ELSE SET msg = 'No such company';
+END IF;
+END //
+
+DELIMITER ;
+
+-- CALL update_football_club('Popov', 'Daniil');
+
---
+++ DB/Lab6/src/main/java/ua/lviv/iot/Application.java
@@ -0,0 +1,395 @@
+package ua.lviv.iot;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Application {
+    private static final String url =
+            "jdbc:mysql://localhost:3306/lab6?serverTimezone=UTC&useSSL=false";
+    private static final String user = "root";
+    private static final String password = "admin";
+
+    private static Connection connection = null;
+    private static Statement statement = null;
+    private static ResultSet rs = null;
+
+    public static void main(String args[]) {
+        try {
+            //This will load the MySQL driver, each DB has its own driver //
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //Get a connection to database //
+            connection = DriverManager.getConnection(url, user, password);
+            // Create a statement
+            // Statements allow to issue SQL queries to the database
+            statement = connection.createStatement();
+
+
+        startApp();
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL Driver is not loaded");
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+
+        } finally {
+            //close connection ,statement and resultset
+            if (rs != null) try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } // ignore
+            if (statement != null) try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private static void startApp() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select your command: ");
+        System.out.println("-----View--------: \n" +
+                "Players: v1\n" +
+                "Clubs: v2\n" +
+                "Companies: v3\n" +
+                "-----Insert--------: \n" +
+                "Players: i1\n" +
+                "Clubs: i2\n" +
+                "Companies: i3\n" +
+                "-----Update--------: \n" +
+                "Players: u1\n" +
+                "Clubs: u2\n" +
+                "Companies: u3\n" +
+                "-----Delete--------: \n" +
+                "Players: d1\n" +
+                "Clubs: d2\n" +
+                "Companies: d3\n" +
+                "-----Insert PlayerCompanies--------: \n" +
+                "ipc");
+
+        String cmd = sc.next();
+
+        if (cmd.equals("v1")) {
+            readPlayers();
+        } else if (cmd.equals("v2")) {
+            readClubs();
+        } else if (cmd.equals("v3")) {
+            readCompanies();
+        } else if (cmd.equals("i1")) {
+            insertFootballPlayer();
+        } else if (cmd.equals("i2")) {
+            insertFootballClub();
+        } else if (cmd.equals("i3")) {
+            insertCompany();
+        } else if (cmd.equals("u1")) {
+            updateFootballPlayer();
+        } else if (cmd.equals("u2")) {
+            updateFootballClub();
+        } else if (cmd.equals("u3")) {
+            updateCompany();
+        } else if (cmd.equals("d1")) {
+            deleteFootballPlayer();
+        } else if (cmd.equals("d2")) {
+            deleteFootballClub();
+        } else if (cmd.equals("d3")) {
+            deleteCompany();
+        } else if (cmd.equals("ipc")) {
+            callProcedureForInsertToPlayersCompanies();
+        } else {
+            System.out.println("Invalid cmd, try again");
+        }
+
+        System.out.println("Procceed? (y/N)");
+        cmd = sc.next();
+        if (cmd.equals("y")) {
+            startApp();
+        } else {
+            System.exit(0);
+        }
+
+    }
+
+
+
+    private static void readPlayers() throws SQLException{
+        rs = statement.executeQuery("SELECT COUNT(*) FROM football_player");
+
+        //Process the result set
+        while (rs.next()) {
+            int count = rs.getInt(1);
+            // Simply Print the results
+            System.out.format("\ncount: %d\n", count);
+        }
+
+
+        rs = statement.executeQuery("SELECT * FROM football_player");
+
+        System.out.format("\n----------------------Table FootballPlayer --------------------\n");
+        System.out.format("%-16s %-16s %-10s %-16s \n", "Last Name", "First Name", "Years XP", "Club");
+        while (rs.next()) {
+            String lastName = rs.getString("last_name");
+            String firstName = rs.getString("first_name");
+            int yearsOfExperience = rs.getInt("years_of_experience");
+            String clubName = rs.getString("club_name");
+            // Simply Print the results
+            System.out.format("%-16s %-16s %-10d %-16s \n", lastName, firstName, yearsOfExperience, clubName);
+        }
+    }
+
+    private static void readClubs() throws SQLException {
+        rs = statement.executeQuery("SELECT * FROM football_club");
+
+        //Process the result set
+        System.out.format("\n---------------------Table FootballClub --------------------\n");
+        System.out.format("%-20s %-16s %-16s %-16s\n", "Club Name", "Club Owner Name", "Coach Name", "Country");
+        while (rs.next()) {
+            String clubName = rs.getString("club_name");
+            String clubOwnerName = rs.getString("club_owner_name");
+            String coachName = rs.getString("coach_name");
+            String country = rs.getString("country");
+            // Simply Print the results
+            System.out.format("%-20s %-16s %-16s %-16s\n", clubName, clubOwnerName, coachName, country);
+        }
+    }
+
+    private static void readCompanies() throws SQLException {
+
+        rs = statement.executeQuery("SELECT * FROM add_company");
+
+        //Process the result set
+        System.out.format("\n-------------------Table Company--------------------\n");
+        System.out.format("%-16s %-16s %-16s %-16s\n", "Name", "Budget", "Industry", "Country");
+        while (rs.next()) {
+            String companyName = rs.getString("company_name");
+            double budget = rs.getDouble("budget");
+            String industry = rs.getString("industry");
+            String country = rs.getString("counry");
+            // Simply Print the results
+            System.out.format("%-16s %-16f %-16s %-16s\n", companyName, budget, industry, country);
+        }
+
+    }
+
+
+    private static void insertFootballPlayer() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a player Last Name: ");
+        String lastName = input.next();
+        System.out.println("Input a player First Name: ");
+        String firstName = input.next();
+        System.out.println("Input a player Experience: ");
+        int exp = input.nextInt();
+        System.out.println("Input a player Club: ");
+        String club = input.next();
+
+        // 3. executing SELECT query
+        //   PreparedStatements can use variables and are more efficient
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement("INSERT INTO football_player VALUES (?, ?, ? , ?)");
+        preparedStatement.setString(1, lastName);
+        preparedStatement.setString(2, firstName);
+        preparedStatement.setInt(3, exp);
+        preparedStatement.setString(4, club);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that inserted: " + n);
+
+    }
+
+    private static void insertFootballClub() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a Club Name: ");
+        String name = input.next();
+        System.out.println("Input a Club Owner: ");
+        String owner = input.next();
+        System.out.println("Input a Club Coach: ");
+        String coach = input.next();
+        System.out.println("Input a Club Country: ");
+        String country = input.next();
+
+        // 3. executing SELECT query
+        //   PreparedStatements can use variables and are more efficient
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement("INSERT INTO football_club VALUES (?, ?, ? , ?)");
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, owner);
+        preparedStatement.setString(3, coach);
+        preparedStatement.setString(4, country);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that inserted: " + n);
+
+    }
+
+    private static void insertCompany() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a Company Name: ");
+        String name = input.next();
+        System.out.println("Input a Company Budget: ");
+        double budget = input.nextDouble();
+        System.out.println("Input a Company Industry: ");
+        String industry = input.next();
+        System.out.println("Input a Company Country: ");
+        String country = input.next();
+
+        // 3. executing SELECT query
+        //   PreparedStatements can use variables and are more efficient
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement("INSERT INTO football_club VALUES (?, ?, ? , ?)");
+        preparedStatement.setString(1, name);
+        preparedStatement.setDouble(2, budget);
+        preparedStatement.setString(3, industry);
+        preparedStatement.setString(4, country);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that inserted: " + n);
+
+    }
+
+
+    private static void updateFootballPlayer() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input Last Name for the player you want to update: ");
+        String lastName = input.next();
+        System.out.println("Input First Name for the player you want to update: ");
+        String firstName = input.next();
+        System.out.println("Input new Last Name: ");
+        String newLastName = input.next();
+        System.out.println("Input new First Name: ");
+        String newFirstName = input.next();
+
+        CallableStatement callableStatement;
+        callableStatement = connection.prepareCall("{CALL update_football_player(?, ?, ?, ?)}");
+        callableStatement.setString(1,lastName);
+        callableStatement.setString(2, firstName);
+        callableStatement.setString(3, newLastName);
+        callableStatement.setString(4, newFirstName);
+        ResultSet rs = callableStatement.executeQuery();
+
+        while (rs.next()) {
+            String msg = rs.getString(1);
+            // Simply Print the results
+            System.out.format("\nResult: " + msg);
+        }
+    }
+
+    private static void updateFootballClub() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input name for the club you want to update: ");
+        String oldName = input.next();
+        System.out.println("Input new club name: ");
+        String newName = input.next();
+
+
+        CallableStatement callableStatement;
+        callableStatement = connection.prepareCall("{CALL update_football_club(?, ?)}");
+        callableStatement.setString(1,oldName);
+        callableStatement.setString(2, newName);
+        ResultSet rs = callableStatement.executeQuery();
+
+        while (rs.next()) {
+            String msg = rs.getString(1);
+            // Simply Print the results
+            System.out.format("\nResult: " + msg);
+        }
+    }
+
+
+    private static void updateCompany() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input name for the company you want to update: ");
+        String oldName = input.next();
+        System.out.println("Input new company name: ");
+        String newName = input.next();
+
+
+        CallableStatement callableStatement;
+        callableStatement = connection.prepareCall("{CALL update_company(?, ?)}");
+        callableStatement.setString(1,oldName);
+        callableStatement.setString(2, newName);
+        ResultSet rs = callableStatement.executeQuery();
+
+        while (rs.next()) {
+            String msg = rs.getString(1);
+            // Simply Print the results
+            System.out.format("\nResult: " + msg);
+        }
+    }
+
+    private static void deleteFootballPlayer() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a last name of player to delete: ");
+        String lastName = input.next();
+        System.out.println("Input a first name of player to delete: ");
+        String firstName = input.next();
+
+
+        //PreparedStatements can use variables and are more efficient
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement("DELETE FROM football_player WHERE last_name=? AND first_name=?");
+        preparedStatement.setString(1, lastName);
+        preparedStatement.setString(2, firstName);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that deleted: " + n);
+    }
+
+    private static void deleteFootballClub() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a name of club to delete: ");
+        String name = input.next();
+
+        //PreparedStatements can use variables and are more efficient
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement("DELETE FROM football_club WHERE club_name=?");
+        preparedStatement.setString(1, name);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that deleted: " + n);
+    }
+
+    private static void deleteCompany() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a name of club to delete: ");
+        String name = input.next();
+
+
+
+        //PreparedStatements can use variables and are more efficient
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement("DELETE FROM add_company WHERE company_name=?");
+        preparedStatement.setString(1, name);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that deleted: " + n);
+    }
+
+    private static void callProcedureForInsertToPlayersCompanies() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nInput Last Name For Player: ");
+        String lastName = input.next();
+        System.out.println("Input First Name For Player: ");
+        String firstName = input.next();
+        System.out.println("Input Company Name: ");
+        String companyName = input.next();
+
+        CallableStatement callableStatement;
+        callableStatement = connection.prepareCall("{CALL insert_playerscompanies(?, ?, ?)}");
+        callableStatement.setString(1, lastName);
+        callableStatement.setString(2, firstName);
+        callableStatement.setString(3, companyName);
+        ResultSet rs = callableStatement.executeQuery();
+
+        while (rs.next()) {
+            String msg = rs.getString(1);
+            // Simply Print the results
+            System.out.format("\nResult: " + msg);
+        }
+    }
+
+}
+
